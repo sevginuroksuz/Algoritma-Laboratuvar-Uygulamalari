c1722924a7b3174284ae26edcd9c14d63f9bdfe8
Sevgi Nur Öksüz <90787721+sevginuroksuz@users.noreply.github.com>
2022-11-12 23:21:56 +0300

    commit6

---
+++ lab2.1.c
@@ -0,0 +1,55 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+int main(){
+    int bugun, gelecek, gelecekGun;
+    printf("Bugun hangi gun?\n");
+    scanf("%d",&bugun);
+    printf("Kac gun sonrasini merak ediyorsunuz?\n",gelecek);
+    scanf("%d",&gelecek);
+    gelecekGun=bugun+gelecek;
+    gelecekGun=gelecekGun%7;
+
+    switch (gelecekGun)
+    {
+        case 1:
+            printf("%d gun sonrasi pazartesi",gelecek);
+                break;
+        case 2:
+             printf("%d gun sonrasi sali",gelecek);
+                 break;
+
+        case 3:
+             printf("%d gun sonrasi carsamba",gelecek);
+                 break;
+        case 4:
+             printf("%d gun sonrasi persembe",gelecek);
+                 break;
+        case 5:
+             printf("%d gun sonrasi cuma",gelecek);
+                 break;
+        case 6:
+             printf("%d gun sonrasi cumartesi",gelecek);
+                 break;
+        case 7:
+             printf("%d gun sonrasi pazar", gelecek);
+                 break;
+        
+
+         default:
+            ;
+            break;
+    }
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
