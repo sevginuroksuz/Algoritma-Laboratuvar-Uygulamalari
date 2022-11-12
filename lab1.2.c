55ff343c4bcfe69a36426b20f0ed2781ffb9546f
Sevgi Nur Öksüz <90787721+sevginuroksuz@users.noreply.github.com>
2022-11-13 00:08:08 +0300

    commit3

--- lab1.2.c
+++ lab1.2.c
@@ -12,16 +12,20 @@ int main(){
      
     printf("Birinci sayiyi giriniz: \n");
     scanf("%f",&x);
+    
     printf("Ikinci sayiyi giriniz: \n");
     scanf("%f",&y);
     
     
     toplama = x+y;
     printf("x+y: %f \n",toplama);
+
     cikarma= x-y;
     printf("x-y: %f\n", cikarma);
+
     carpma= x*y;
     printf("x*y: %f \n",carpma);
+
     bolme=x/y;
     printf("x/y: %f\n",bolme);
 
